61ab633b8f4bcd7589bbad44084c8a15c13a375d
Emin Okic <emin.okic1729@gmail.com>
2021-10-27 03:58:05 -0500

    finishing up containerd program

--- containerd_getting_started/main.go
+++ containerd_getting_started/main.go
@@ -1,25 +1,32 @@
 package main
 
-import "fmt"
 import "log"
 import "github.com/containerd/containerd"
 
-func main() {{
+func main() {
     if err := redisExample(); err != nil {
         log.Fatal(err)
     } // Close if statement.
 } // Close main function block.
 
 func redisExample() error {
+    client, err := containerd.New("/run/containerd/containerd.sock")
     
-  client, err := containerd.New("run/containerd/containerd.sock")
-     
-  if err != nil {
-    return err
-  } // Close if statement
-   
-  defer client.Close()
+    if err != nil {
+        return err
+    }
     
-  return nil
+    defer client.Close()
+
+    ctx := namespace.WithNamespace(context.Background(), "example")
+    image, err := client.Pull(ctx, "docker.io/libraryredis:alpine", containerd.WithPullUnpack)
+    
+    if err != nil {
+        return err
+    }
+    
+    log.Printf("Successfully pulled %s image\n", image.Name())
+    
+    return nil
 
 } // Close redisExample function block.
